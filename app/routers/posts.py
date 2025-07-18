d2929a44c1062ccda108bc169f97309b0683cecf
Adil-Bee200 <151982354+Adil-Bee200@users.noreply.github.com>
2025-07-17 19:31:41 -0700

    Updated posts

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import joinedload
 
 router = APIRouter(prefix="/posts", tags= ['posts'])
 
-@router.get("/", response_model=list[schema.PostWithVotes])
+@router.get("", response_model=list[schema.PostWithVotes])
 def get_posts(db: Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user), limit : int = 10,
               skip : int = 0, search : Optional[str] = ""):
     
